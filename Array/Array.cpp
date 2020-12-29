2a509e92d39447f5f04c583349dadcb7c9785913
girishsontakke <girishsontakke7@gmail.com>
2020-12-29 21:30:24 +0530

    append and insert in Array

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -5,10 +5,12 @@ class Array
 {
 public:
   t *A;
-  int size;
-  int length;
+  int size = 0;
+  int length = 0;
   Array(){};
   void display();
+  void append(t value);
+  void insert(t index, t value);
 };
 
 int main()
@@ -17,25 +19,76 @@ int main()
   std::cout << "Enter the size of array\n";
   std::cin >> a.size;
   a.A = new int[a.size];
-  a.length = 0;
-
   int i;
-  do{
-    std::cout<<"Enter the number of elements\n";
-    std::cin >>i;
-  }while(i>a.size);
 
-  std::cout<<"Enter the elements\n";
-  for(int j=0;j<i; j++){
-    std::cin>>a.A[j];
+  do
+  {
+    std::cout << "Enter the number of elements\n";
+    std::cin >> i;
+  } while (i > a.size);
+
+  std::cout << "Enter the elements\n";
+  for (int j = 0; j < i; j++)
+  {
+    std::cin >> a.A[j];
     a.length++;
   }
+  a.insert(0, 1);
   a.display();
 }
 
-template<typename t>
-void Array<t>::display(){
-  for(int i=0; i<Array<t>::length; i++)
-    std::cout<< Array<t>::A[i] << " ";
-  std::cout<<"\n";
+template <typename t>
+void Array<t>::display()
+{
+  for (int i = 0; i < Array<t>::length; i++)
+    std::cout << Array<t>::A[i] << " ";
+  std::cout << "\n";
+}
+
+template <typename t>
+void Array<t>::append(t value)
+{
+  if (Array<t>::length < Array<t>::size)
+  {
+    Array<t>::A[Array<t>::length++] = value;
+  }
+  else
+  {
+    (Array<t>::size == 0) ? Array<t>::size = 1 : Array<t>::size *= 2;
+    t *B = new int[Array<t>::size];
+    for (int i = 0; i < Array<t>::length; i++)
+    {
+      B[i] = A[i];
+    }
+    B[Array<t>::length++] = value;
+    Array<t>::A = B;
+  }
+}
+
+template <typename t>
+void Array<t>::insert(t index, t value)
+{
+  if (index < 0 || index > Array<t>::length)
+    return;
+  if (Array<t>::length < Array<t>::size)
+  {
+    for (int i = Array<t>::length; i > index; i--)
+      Array<t>::A[i] = Array<t>::A[i - 1];
+    Array<t>::A[index] = value;
+    length++;
+  }
+  else
+  {
+    (Array<t>::size == 0) ? Array<t>::size = 1 : Array<t>::size *= 2;
+    t *B = new int[Array<t>::size];
+    for (int i = 0; i < Array<t>::length; i++)
+    {
+      B[i] = A[i];
+    }
+    Array<t>::A = B;
+    for (int i = Array<t>::length; i > index; i--)
+      Array<t>::A[i] = Array<t>::A[i - 1];
+    Array<t>::A[index] = value;
+    length++;
+  }
 }
